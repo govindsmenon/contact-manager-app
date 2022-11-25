6f5a89ef2af85fb46a28312370a5912cbe2d43db
Govind S Menon <govindsmenon926@gmail.com>
2022-11-25 23:36:09 +0530

    added loading spinner, db.json, logic for contact manager, edit contact, view contact, contact service

--- src/app/components/view-contact/view-contact.component.ts
+++ src/app/components/view-contact/view-contact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { IContact } from 'src/app/models/iContact';
+import { IGroup } from 'src/app/models/iGroup';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-view-contact',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewContactComponent implements OnInit {
 
-  constructor() { }
+  public contactId: string | null = null;
+  public loading: boolean = false;
+  public contact: IContact = {} as IContact;
+  public errorMessage: string | null = null;
+  public group: IGroup = {} as IGroup;
+
+  constructor(private contactService: ContactService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe( (param: ParamMap) => {
+      this.contactId = param.get('contactId');
+    })
+    if(this.contactId){
+      this.loading = false;
+      this.contactService.getContact(this.contactId).subscribe((data: IContact)=>{
+        this.contact = data;
+        this.loading = false;
+        this.contactService.getGroup(data).subscribe((data: IGroup)=>{
+          this.group = data;
+        }, (error) => {
+          this.errorMessage = error;
+          this.loading = false;
+        });
+      }, (error) => {
+        this.errorMessage = error;
+        this.loading = false;
+      });
+    }
+  }
+
+  public isNotEmpty(){
+    return Object.keys(this.contact).length > 0 && Object.keys(this.group).length > 0;;
   }
 
 }
